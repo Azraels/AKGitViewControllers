142cb29acf1f2bccdada99834c7648908ecfc4f3
jim <jim122664@gmail.com>
2018-05-03 18:59:30 +0800

    add test file

---
+++ AKGitViewControllers/Classes/AKGitViewControllers.h
@@ -0,0 +1,26 @@
+//  AKGitViewControllers.h
+#import <UIKit/UIKit.h>
+
+@interface AKGitViewControllers : UIViewController
+    
+- (void)placeALabelOnVC;
+    
+    @end
+
+//  AKGitViewControllers.m
+#import "AKGitViewControllers.h"
+
+@implementation AKGitViewControllers
+    
+- (void)viewDidLoad {
+    [self placeALabelOnVC];
+}
+    
+- (void)placeALabelOnVC{
+    UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height/2, self.view.frame.size.width, 20)];
+    lbl.textAlignment = NSTextAlignmentCenter;
+    lbl.text = @"This is a testing of pods";
+    [self.view addSubview:lbl];
+}
+    
+    @end
